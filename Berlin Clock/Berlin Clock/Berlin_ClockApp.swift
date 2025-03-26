ff766cf2868a3ac380287af12786ba6a3c893ce2
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-03-26 14:36:43 +0100

    Create project

---
+++ Berlin Clock/Berlin Clock/Berlin_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berlin_ClockApp.swift
+//  Berlin Clock
+//
+//  Created by Адлет Жумагалиев on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Berlin_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
